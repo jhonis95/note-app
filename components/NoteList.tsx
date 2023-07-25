ccb880f47f897f81d37aac4256df74f5530f68df
jonatan de oliveira <j.jhonis95@gmail.com>
2023-07-24 23:03:47 -0300

    adding login page

---
+++ components/NoteList.tsx
@@ -0,0 +1,7 @@
+export default function NodeList(){
+    return(
+        <>
+            
+        </>
+    )
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function login(){
+    return(
+        <>
+            <h1>loggin page</h1>
+        </>
+    )
+}
