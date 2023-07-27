04756ccdb001928d2eb61e330c94a4bdb158fe59
jonatan de oliveira <j.jhonis95@gmail.com>
2023-07-26 22:33:30 -0300

    adding the global typescript file to the project

---
+++ types/global.d.ts
@@ -0,0 +1,11 @@
+//for global types for the project
+export {}
+
+declare global {
+    //add the types here
+//   interface IPost {
+//     id: number
+//     title: string
+//     body: string
+//   }
+}
